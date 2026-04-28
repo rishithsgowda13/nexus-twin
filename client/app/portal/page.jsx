b56670953411c39bf59b003dde4419226ca60421
bharathkumar000 <bharathece2006@gmail.com>
2026-04-29 01:54:46 +0530

    feat: implemented next.js app router architecture for role-based portal access

---
+++ client/app/portal/page.jsx
@@ -0,0 +1,6 @@
+'use client';
+import PortalPage from '../../components/PortalPage';
+
+export default function Portal() {
+  return <PortalPage />;
+}
---
+++ client/app/user/page.jsx
@@ -0,0 +1,6 @@
+'use client';
+import UserDashboard from '../../components/UserDashboard';
+
+export default function User() {
+  return <UserDashboard />;
+}
