af1c3825bb99bca75f342c86ebcef597b57c4ca6
rishithsgowda13 <rishithsgowda22@gmail.com>
2026-04-29 05:46:09 +0530

    Add/Update client/app/portal/page.jsx

--- client/app/portal/page.jsx
+++ client/app/portal/page.jsx
@@ -1,5 +1,5 @@
 'use client';
-import PortalPage from '../../../shared/PortalPage';
+import PortalPage from '../../shared/PortalPage';
 
 export default function Portal() {
   return <PortalPage />;
